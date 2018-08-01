8a4b228a223f487a81a60f1402c88768cd2f48d2
Baha Ganyev <ganyev7@gmail.com>
2018-08-01 21:56:39 +0600

    проверил ServerManager

--- JardamBeremNew/Network/ServerManager.swift
+++ JardamBeremNew/Network/ServerManager.swift
@@ -29,26 +29,10 @@ class ServerManager: HTTPRequestManager {
         }
     }
     
-    func getCategories(completion: @escaping ([Category]) -> (), error: @escaping (String) -> ()) {
-        self.get(endpoint: Constants.Network.EndPoint.categories, completion: { (data) in
-            //TODO
-            do {
-                guard let  data = data else { return }
-                let result = try JSONDecoder().decode(CategoryResult.self, from: data)
-                completion(result.results)
-            }
-            catch let errorMessage {
-                error(errorMessage.localizedDescription)
-            }
-            
-        }) { (errorMessage) in
-            error(errorMessage)
-        }
-    }
     
-    func getCategories(id_of_city: String, completion: @escaping ([Category]) -> (), error: @escaping (String) -> ()) {
-        self.get(endpoint: "\(Constants.Network.EndPoint.categories)/(id_of_city)/categories", completion: { (data) in
-            //TODO
+    func getCategories(id: Int, completion: @escaping ([Category]) -> (), error: @escaping (String) -> ()) {
+        self.get(endpoint: Constants.Network.EndPoint.category(by: id), completion: { (data) in
+  
             do {
                 guard let  data = data else { return }
                 let result = try JSONDecoder().decode(CategoryResult.self, from: data)
--- JardamBeremNew/ViewController.swift
+++ JardamBeremNew/ViewController.swift
@@ -12,14 +12,34 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ServerManager.shared.getCities(completion: printCities) { (error) in
+            print(error)
+        }
+        ServerManager.shared.getCategories(id: 1, completion: printCategories) { (error) in
+            print(error)
+        }
+        ServerManager.shared.getReviews(completion: printReviews) { (error) in
+            print(error)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func printCities(city: [City]) {
+        for i in city {
+            print(i.city_name ?? "")
+        }
     }
-
+    
+    func printCategories(category: [Category]) {
+        for i in category {
+            print(i.category_name ?? "", i.city ?? "", i.id ?? "")
+        }
+    }
+    
+    func printReviews(review: [Review]) {
+        for i in review {
+            print(i.email ?? "", i.id ?? "", i.reviewText ?? "")
+        }
+    }
 
 }
 
