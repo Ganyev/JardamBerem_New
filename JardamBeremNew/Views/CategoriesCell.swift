f91ab9e4e1d34a8eb4ff11a0c1a0da758555cd76
Baha Ganyev <ganyev7@gmail.com>
2018-08-02 00:52:51 +0600

    добавил CategoriesCell

---
+++ JardamBeremNew/Views/CategoriesCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoriesCell.swift
+//  JardamBeremNew
+//
+//  Created by Baha Ganyev on 02.08.2018.
+//  Copyright © 2018 Baha Ganyev. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesCell: UICollectionViewCell {
+    
+}
