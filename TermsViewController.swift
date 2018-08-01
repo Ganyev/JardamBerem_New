63a6e3da7841bcb61e7b85c1148c0f8678823d2b
Ramilia3110 <ramilia3110@gmail.com>
2018-08-01 08:54:12 -0700

    Ramilias storyboard and ViewController added

---
+++ TermsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TermsViewController.swift
+//  JardamBeremNew
+//
+//  Created by Ramilia Imankulova on 8/1/18.
+//  Copyright © 2018 Baha Ganyev. All rights reserved.
+//
+
+import UIKit
+
+class TermsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
