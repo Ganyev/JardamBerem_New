02c2cd67f9a14ccc7a97d5ceede48f734ba159d1
Baha Ganyev <ganyev7@gmail.com>
2018-08-02 15:29:18 +0600

    created ForumViewController

---
+++ JardamBeremNew/ViewControllers/ForumViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ForumViewController.swift
+//  JardamBeremNew
+//
+//  Created by Baha Ganyev on 02.08.2018.
+//  Copyright © 2018 Baha Ganyev. All rights reserved.
+//
+
+import UIKit
+
+class ForumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
